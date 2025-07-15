cac9f45c8c845680f687d5f42c6004a777876eec
JesseMurah <jkhnmurah@gmail.com>
2025-07-15 07:01:40 +0000

    [docker] made changes to the .dev

--- voting-fronend-1/components/nomination-form.tsx
+++ voting-fronend-1/components/nomination-form.tsx
@@ -195,7 +195,7 @@ export function NominationForm() {
 
       console.log("Sending payload:", payload);
 
-      const response = await axios.post("http://localhost:3000/api/v1/nominations", payload, {
+      const response = await axios.post("https://pax-romana-vote-production.up.railway.app/api/v1/nominations", payload, {
         headers: {
           "Content-Type": "application/json"
         },
